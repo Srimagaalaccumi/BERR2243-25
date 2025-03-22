38c3d525441d71b29ab0d0ac677160b8db5a4644
Srimagaalaccumi <b122410305@student.utem.edu.my>
2025-03-23 04:03:12 +0800

    added index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const {MongoClient} = require('mongodb');
+
+async function main()
+ {
+    //Replace <connection-string> with your MongoDB URI
+    const uri = "mongobd://localhost:27017"
+    const client = new MongoClient(uri);
+
+    try
+    {
+        await client .connect();
+        console.log("Connected to MongoDB!");
+
+        const db = client.db ("testDB");
+        const collection = db.collection("users");
+
+        //Insert a document
+        await collection.insertOne({name: "Alice", age: 25});
+        console.log ("Document inserted!");
+
+       //Query the document
+       const result = await collection.findOne({ name: "Alice"});
+       console.log("Query result:", result);
+    } 
+    catch (err) 
+    {
+        console.error("Error:", err);
+    }
+    finally
+    {
+        await client.close();
+    }
+}
+
+main();
