f0e8838243d671a4c9c846d469947e84eb42547e
Srimagaalaccumi <b122410305@student.utem.edu.my>
2025-04-02 19:45:15 +0800

    Restored index.js in second-branch

--- index.js
+++ index.js
@@ -31,3 +31,64 @@ async function main() {
 }
 
 main();
+
+const { MongoClient } = require('mongodb');
+
+const drivers = [
+    {
+        name: "John Doe",
+        vehicleType: "Sedan",
+        isAvailable: true,
+        rating: 4.8
+    },
+    {
+        name: "Alice Smith",
+        vechileType: "SUV",
+        isAvailable: false,
+        rating: 4.5
+    },
+    {
+        name: "Kelvin",
+        vechileType: "BMW",
+        isAvailable: true,
+        rating: 4.0
+    },
+    {
+        name: "Mark Benjamin",
+        vechileType: "VOLVO",
+        isAvailable: false,
+        rating: 4.9
+    }
+
+];
+
+//Show the data in the console
+console.log(drivers);
+
+
+async function main() {
+    //Replace <connection-string> with your MongoDB URI
+    const uri = "mongodb://localhost:27017"
+    const client = new MongoClient(uri);
+
+    console.time("MongoDB Connection Time"); // Start time tracking
+
+    try {
+        
+        await client.connect();        
+        const db = client.db ("testDB");
+
+        const driversCollection = db.collection("drivers");
+
+        drivers.forEach(async (driver) => {
+            const result = await driversCollection.insertOne(driver);
+            console.log('New driver created with result: ${result}');
+        });
+
+    } finally{
+        await client.close();
+    }
+}
+
+main();
+
