5f3c819c79880eac96a8229d8a02507957291bcc
Srimagaalaccumi <b122410305@student.utem.edu.my>
2025-04-03 21:48:32 +0800

    added WEEK2.js

--- WEEK2.js
+++ WEEK2.js
@@ -47,16 +47,17 @@ async function main() {
         console.log(`New driver created with result: ${result}`);
     });   
 
-    const updateResult = await db.collection('drivers').updateOne(
-        {name: "John Doe"},
-        {$inc: {rating: 0.1} }
-    );
-    console.log(`Driver updated with result: ${updateResult}`);
+    const availableDrivers = await db.collection('drivers').find  ({
+      isAvailable: true,
+      rating: {$gte: 4.5 }
+    }).toArray();
 
-    // Fetch and log John Doe's updated data
-    const updatedJohnDoe = await db.collection('drivers').findOne({ name: "John Doe" });
-    console.log("John Doe's updated data:", updatedJohnDoe);
-
+    const updateResult = await db.collection('drivers').updateMany(
+      { isAvailable: true }, 
+      { $set: { rating: 0.1 } }
+  );
+  console.log(`Drivers updated with result: ${updateResult}`);
+  
 
     const deleteResult = await db.collection('drivers').deleteMany({ isAvailable: false });
     console.log(`Driver deleted with result: ${deleteResult.deletedCount}`);
