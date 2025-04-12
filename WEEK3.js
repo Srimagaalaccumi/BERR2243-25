2cdd66f3bb4e4c5ac858a1847c172fadbc7d74a1
Srimagaalaccumi <b122410305@student.utem.edu.my>
2025-04-13 00:15:07 +0800

    Initial commit with WEEK3.js

--- WEEK3.js
+++ WEEK3.js
@@ -1,2 +1,39 @@
-console.log("Hello from third branch!");
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const port = 3000;
 
+const app = express();
+app.use(express.json());
+
+let db;
+
+async function connectToMongoDB() {
+    const uri = "mongodb://localhost:27017";
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+
+        db = client.db("testDB");
+    } catch (err) {
+        console.error("Error:", err);
+    }
+}
+connectToMongoDB();
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+//GET /rides - fetch all rides
+app.get('/rides', async (req, res) =>{
+    try {
+        const rides = await db.collection('rides').find().toArray();
+        res.status(200).json(rides);
+
+    } catch (err) {
+        res.status(500).json({ error: "Failed to fetch rides"});
+    }
+
+});
