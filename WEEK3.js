c9557d6ed470317432ff1c28990bf83ee833339b
Srimagaalaccumi <b122410305@student.utem.edu.my>
2025-04-13 00:56:04 +0800

    Initial commit with WEEK3.js

--- WEEK3.js
+++ WEEK3.js
@@ -37,3 +37,15 @@ app.get('/rides', async (req, res) =>{
     }
 
 });
+
+//POST/Rides - Create a new rides
+app.post('/rides', async (req, res) =>{
+    try {
+        const result = await db.collection('rides').insertOne(req.body);
+        res.status(201).json({ id: result.insertedId});
+
+    } catch (err) {
+        res.status(400).json({ error: "Invalid ride data"});
+    }
+
+});
